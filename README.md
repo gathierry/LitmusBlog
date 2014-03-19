4a02d36e939782a3957bd23fd2fc39c56529ccf0
李石羽 <ligary04@hotmail.com>
2014-03-20 00:38:29 +0800

    New look of chapter 1
    - update Readme
    - add connect-flash

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ LitmusBlog
 ==========
 
 A blog based on node.js and jade engine
+
+# 路由规划：
+/ : 首页
+/post : 发表文章
+/blog : 博文列表
+/blog/title : title为标题的文章
--- settings.js
+++ settings.js
@@ -1,1 +1,1 @@
-﻿module.exports = {  cookieSecret: 'litmus-blog',  db: 'litmus_blog',  host: 'localhost'};
+﻿module.exports = {  cookieSecret: 'litmus-blog',  db: 'litmus_blog',  host: 'localhost'};//db 是数据库的名称，host 是数据库的地址。cookieSecret 用于 Cookie 加密与数据库无关
