527116d32b97041c4dcb6bbe19a34f51a5aac787
李石羽 <ligary04@hotmail.com>
2014-08-10 11:28:21 +0800

    Change to Shiyu's Blog
    - Home
    - Private Property
    - Markdown
    - List
    - Admin

--- README.md
+++ README.md
@@ -1,10 +1,34 @@
-LitmusBlog
-==========
+#LitmusBlog
 
-A blog based on node.js and jade engine
+
+A blog based on node.js, jade and bootstrap.
 
 # 路由规划：
-/ : 首页
-/post : 发表文章
-/blog : 博文列表
-/blog/title : title为标题的文章
+/ : 首页  
+/post : 发表文章  
+/blog : 博文列表  
+/blog/title : title为标题的文章  
+
+===
+#Shiyu's Blog
+
+##Data Model
+
+- Post
+  - title
+  - post
+  - time
+  - image
+  - private (tag)
+
+##Routes
+/ : Home  
+/my-blog : MY BLOG
+/list : LIST
+/about-me : ABOUT ME  
+/contact : CONTACT
+
+===
+
+#Change Log
+- bootstrap.css中标题与正文字体加入“Georgia”和“Cambria”
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,214 @@
-﻿/* * GET home page. */Post = require('../models/post.js');Modify = require('../models/modify.js');var fs = require('fs');var tags = ["Life in France", "Computer Science", "Reading & Writing"]; var timelines;/* req.query： 处理 get 请求，获取 get 请求参数 req.params： 处理 /:xxx 形式的 get 或 post 请求，获取请求参数 req.body： 处理 post 请求，获取 post 请求体 req.param()： 处理 get 和 post 请求，但查找优先级由高到低为 req.params→req.body→req.query */module.exports = function(app){  app.get('/', function(req, res){      res.render('index', {           title: 'Litmus Blog',          tags:tags,          timelines:timelines,      });  });  app.get('/blog', function(req, res){        var page = req.query.p;      if(!page) page = 1;      else page = parseInt(page);      Post.getTen(page, null, function(err, posts){          if (err) posts = [];          res.render('blog', {              title:'Blog',              page:page,              posts:posts,              tags:tags,              timelines:timelines,          });      });  });  app.get('/tag/:tag', function(req, res){        var page = req.query.p;      if(!page) page = 1;      else page = parseInt(page);      var tag = req.params.tag;      Post.getTen(page, tag, function(err, posts){          if (err) posts = [];                  res.render('blog', {              title:'Blog',              page:page,              posts:posts,              postsLen:posts.length,              tags:tags,              timelines:timelines,          });      });  });    app.get('/blog/:title', function(req, res){      Post.getOne(req.params.title, function(err, post){          if (err) post = [];          res.render('article', {              title:req.params.title,              post:post,              tags:tags,              timelines:timelines,          });      });  });  app.get('/post', function(req, res){      res.render('post', {          timelines:timelines,          tags:tags,          title:'New Blog',      });  });  app.post('/post', function(req, res){          //upload image     var imgSrc = '';     var image = req.files.cImage;     if (image.size > 0) imgSrc = image.path;     else fs.unlinkSync(image.path);     var post = new Post(req.body.title, req.body.post, new Date(), req.body.tag, imgSrc);     if (req.body.title.length || req.body.post.length) {         post.save(function(err) {             return res.redirect('/blog');         });     }   });  app.get('/blog/:title/delete', function(req, res){      Modify.deleteArticle(req.params.title, function(err){          return res.redirect('/blog');            });      });  app.get('/blog/:title/update', function(req, res){      Post.getOne(req.params.title, function(err, post){          if (err) post = [];          res.render('post', {              title:req.params.title,              post:post,              tags:tags,              timelines:timelines,          });      });         });           app.post('/blog/:title/update', function(req, res){      var imgSrc = '';     var image = req.files.cImage;     if (image.size > 0) imgSrc = image.path;     else fs.unlinkSync(image.path);     var post = new Post(req.body.title, req.body.post, new Date(), req.body.tag, imgSrc);      if (req.body.title.length || req.body.post.length) {          post.save(function(err) {              return res.redirect('/blog');          });      }   });        app.get('/search', function(req, res){      var page = req.query.p;      if (!page) page = 1;      else page = parseInt(page);      Post.search(req.query.keyword, page, function (err, posts) {          if (err) posts = [];          res.render('blog',{              title:'Search',              page:page,              posts:posts,              tags:tags,              keyword:req.query.keyword,              timelines:timelines,          });      });   });      app.get('/about-me', function(req,  res) {       res.render('aboutMe', {           title:'About me',           tags:tags,           timelines:timelines,       });   });  app.get('/my-apps', function(req,  res) {       res.render('myApps', {           title:'My apps',           tags:tags,           timelines:timelines,       });   });};
+/*
+ * GET home page.
+ */
+
+
+Post = require('../models/post.js');
+var fs = require('fs');
+var crypto = require('crypto');
+
+var prive = null;
+/*
+   req.query： 处理 get 请求，获取 get 请求参数
+   req.params： 处理 /:xxx 形式的 get 或 post 请求，获取请求参数
+   req.body： 处理 post 请求，获取 post 请求体
+   req.param()： 处理 get 和 post 请求，但查找优先级由高到低为 req.params→req.body→req.query
+*/
+
+module.exports = function(app){
+    app.get('/', checkAdmin);
+    app.get('/', function(req, res){
+        Post.getTen(1, prive, function(err, posts){
+            if (err) posts = [];
+            Post.getOne(posts[0].title, function(err, post){
+                if (err) post = [];
+                posts[0] = post;
+                Post.getRecent(prive, function(err, recentPosts){
+                    if (err) recentPosts = [];
+                    res.render('index', { 
+                        title:'Shiyu\'s Blog | Li Shiyu Blog',
+                        posts:posts,
+                        recentPosts:recentPosts,
+                    });
+
+
+                });
+
+            });
+        });
+
+    });
+
+    app.get('/blog', checkAdmin);
+    app.get('/blog', function(req, res){  
+        var page = req.query.p;
+        if(!page) page = 1;
+        else page = parseInt(page);
+        Post.getTen(page, prive, function(err, posts){
+            if (err) posts = [];
+            Post.getRecent(prive, function(err, recentPosts){
+                if (err) recentPosts = [];
+                res.render('blog', {
+                    title:'Blog',
+                    page:page,
+                    posts:posts,
+                    recentPosts:recentPosts,
+                });
+            });
+        });
+    });
+
+    app.get('/blog/:title', checkAdmin);
+    app.get('/blog/:title', function(req, res){
+        Post.getOne(req.params.title, function(err, post){
+            if (err) post = [];
+            Post.getRecent(prive, function(err, recentPosts){
+                if (err) recentPosts = [];
+                res.render('article', {
+                    title:req.params.title,
+                    post:post,
+                    recentPosts:recentPosts,
+                });
+            });
+        });
+    });
+
+    app.get('/post', checkAdmin);
+    app.get('/post', function(req, res){
+        if (req.session.user != 'admin') return res.redirect('/login');
+        Post.getRecent(prive, function(err, recentPosts){
+            if (err) recentPosts = [];
+            res.render('post', {
+                title:'New Blog',
+                recentPosts:recentPosts,
+            });
+        });
+    });
+
+    app.post('/post', function(req, res){     
+        //upload image
+        var imgSrc = '';
+        var image = req.files.cImage;
+        if (image.size > 0) imgSrc = image.path;
+        else fs.unlinkSync(image.path);
+        var post = new Post(req.body.title, req.body.post, new Date(), req.body.tag, imgSrc);
+        if (req.body.title.length || req.body.post.length) {
+            post.save(function(err) {
+                return res.redirect('/blog');
+            });
+        }
+    });
+
+    app.get('/blog/:title/delete', checkAdmin);
+    app.get('/blog/:title/delete', function(req, res){
+        Post.deleteArticle(req.params.title, function(err){
+            if (req.session.user != 'admin') return res.redirect('/login');
+            return res.redirect('/blog');      
+        });    
+    });
+
+    app.get('/blog/:title/update', checkAdmin);
+    app.get('/blog/:title/update', function(req, res){
+        if (req.session.user != 'admin') return res.redirect('/login');
+        Post.updateOne(req.params.title, function(err, post){
+            if (err) post = [];
+            Post.getRecent(prive, function(err, recentPosts){
+                if (err) recentPosts = [];
+                res.render('post', {
+                    title:req.params.title,
+                    post:post,
+                    recentPosts:recentPosts,
+                });
+            });
+        });      
+    });      
+
+    app.post('/blog/:title/update', function(req, res){
+        var imgSrc = '';
+        var image = req.files.cImage;
+        if (image.size > 0) imgSrc = image.path;
+        else fs.unlinkSync(image.path);
+        var post = new Post(req.body.title, req.body.post, new Date(), req.body.tag, imgSrc);
+        if (req.body.title.length || req.body.post.length) {
+            post.save(function(err) {
+                return res.redirect('/blog');
+            });
+        }
+    });     
+
+    app.get('/search', checkAdmin);
+    app.get('/search', function(req, res){
+        var page = req.query.p;
+        if (!page) page = 1;
+        else page = parseInt(page);
+        Post.search(req.query.keyword, page, function (err, posts) {
+            if (err) posts = [];
+            Post.getRecent(prive, function(err, recentPosts){
+                if (err) recentPosts = [];
+                res.render('blog',{
+                    title:'Search',
+                    page:page,
+                    posts:posts,
+                    keyword:req.query.keyword,
+                    recentPosts:recentPosts,
+                });
+            });
+        });
+    });
+
+    app.get('/list', checkAdmin);
+    app.get('/list', function(req, res) {
+        var page = req.query.p;
+        if(!page) page = 1;
+        else page = parseInt(page);
+        Post.getList(page, prive, function(err, posts){
+            if (err) posts = [];
+            Post.getRecent(prive, function(err, recentPosts){
+                if (err) recentPosts = [];
+                res.render('list', {
+                    title:'List',
+                    page:page,
+                    posts:posts,
+                    recentPosts:recentPosts,
+                });
+            });
+        });
+    });
+
+    app.get('/about-me', checkAdmin);
+    app.get('/about-me', function(req,  res) {
+        Post.getRecent(prive, function(err, recentPosts){
+            if (err) recentPosts = [];
+            res.render('aboutMe', {
+                title:'About me',
+                recentPosts:recentPosts,
+            });
+        });
+    });
+
+    app.get('/login', function(req, res) {
+        res.render('login', {
+            title:'Administration Login',
+        });
+    });
+
+    app.get('/logout', function(req, res) {
+        req.session.user = null;
+        res.redirect('/');
+    });
+
+    app.post('/login', function(req, res){
+        if (req.body.username == 'litmus' && req.body.password == '10241072') {
+            req.session.user = 'admin';
+            return res.redirect('/');
+        }
+        return res.redirect('/login');
+    });
+    
+    function checkAdmin(req, res, next){
+        prive = (req.session.user == 'admin');
+        next();
+    }
+    
+};
+
