2915060001c87ab6d404f8c6b32439bbfd8ef81a
李石羽 <ligary04@hotmail.com>
2014-08-11 15:02:40 +0800

    add disqus

--- app.js
+++ app.js
@@ -21,13 +21,13 @@ app.set('view engine', 'jade');
 app.use(express.favicon(__dirname+'/public/img/icon.ico'));//connect 内建的中间件，使用默认的 favicon 图标，如果想使用自己的图标，需改为 app.use(express.favicon(__dirname + '/public/images/favicon.ico')); 这里我们把自定义的 favicon.ico 放到了 /public/images 文件夹下。
 app.use(express.logger('dev'));//在终端显示简单的日志
 app.use(express.bodyParser({ keepExtensions: true, uploadDir: './public/images' }));//用来解析请求体，支持 application/json， application/x-www-form-urlencoded, 和 multipart/form-data
-app.use(express.methodOverride())//connect 内建的中间件，可以协助处理 POST 请求，伪装 PUT、DELETE 和其他 HTTP 方法
+app.use(express.methodOverride());//connect 内建的中间件，可以协助处理 POST 请求，伪装 PUT、DELETE 和其他 HTTP 方法
 app.use(express.cookieParser());
 app.use(express.session({
     secret: settings.cookieSecret,
-       key: settings.db,//cookie name
+    key: settings.db,//cookie name
     cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
-     store: new MongoStore({
+    store: new MongoStore({
         db: settings.db
     })
 }));
